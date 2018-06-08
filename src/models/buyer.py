3e142e41f029b12dc18e12d4959d1dac6ef88f8d
Daniel <tres.daniel.s@gmail.com>
2018-06-07 22:33:58 -0300

    Added methods to buyer class

--- src/models/buyer.py
+++ src/models/buyer.py
@@ -1,3 +1,38 @@
+import uuid
+from src.common.database import Database
+
 
 class Buyer(object):
-    pass
+    def __init__(self, client_id, name, email, cpf, _id=None):
+        self.client_id = client_id,
+        self.name = name,
+        self.email = email,
+        self.cpf = cpf,
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    def save(self):
+        Database.insert(collection='buyers', 
+                        data=self.json())
+
+    def json(self):
+        return {
+            'client_id': self.client_id,
+            'name': self.name,
+            'email': self.email,
+            'cpf': self.cpf,
+            '_id': self._id
+        }
+    
+    @classmethod
+    def from_db(cls, id):
+        client_data = Database.find_one(collection='buyers', query={'_id': id})
+        return cls(**client_data)
+
+    @classmethod
+    def from_client(cls, id):
+        buyer_data = Database.find_one(collection='buyers', query={'client_id': id})
+        return cls(**buyer_data)
+
+    @staticmethod
+    def list_all(id):
+        return [client for client in Database.find(collection='buyers', query={'buyer_id' : id})]

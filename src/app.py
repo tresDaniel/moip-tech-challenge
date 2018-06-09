961908332c7aa0c70e170395949b788434d7ab98
Daniel <tres.daniel.s@gmail.com>
2018-06-09 11:27:30 -0300

    Organize app initilializing classes

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from src.common.database import Database
 
 app = Flask(__name__)
+app.config.from_object('config')
 
-@app.route("/api/")
-def main_method():
-    return "It works!"
-
-if(__name__ == '__main__'):
-    app.run()
+@app.before_first_request
+def init_db():
+    Database.initialize()
